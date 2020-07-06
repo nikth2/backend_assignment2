e32c1c1745b938eaff34d9bdcd7bda2041c9f562
Nikos Theodoropoulos <ntheodor@cosmote.gr>
2020-07-06 10:05:40 +0300

    2nd commit

--- src/main/java/com/cosmote/training/springboot/SpringbootApplication.java
+++ src/main/java/com/cosmote/training/springboot/SpringbootApplication.java
@@ -1,13 +1,18 @@
 package com.cosmote.training.springboot;
 
+import com.cosmote.training.springboot.entities.User;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
 
 @SpringBootApplication
 public class SpringbootApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(SpringbootApplication.class, args);
+		ApplicationContext ctx = new ClassPathXmlApplicationContext("application-context.xml");
+		User user = (User) ctx.getBean("user");
 	}
 
 }
